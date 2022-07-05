5885e698d09388bf0b058a6d69fbed11622142fc
mateusdayrell <mateusdayrell7@gmail.com>
2022-07-05 09:17:38 -0300

    feat: add contato routes

--- routes.js
+++ routes.js
@@ -4,10 +4,21 @@ const route = express.Router()
 const homeController = require('./src/controllers/homeController')
 const loginController = require('./src/controllers/loginController')
 const userController = require('./src/controllers/userController')
+const contatoController = require('./src/controllers/contatoController')
+
+const { requiredLogin } = require('./src/middlewares/middleware')
+
 
 // HOME
 route.get('/', homeController.index)
 route.post('/', homeController.store)
+
+// CONTATO
+route.get('/', contatoController.index)
+route.get('/contato/create', requiredLogin, contatoController.create)
+route.post('/contato/store', requiredLogin, contatoController.store)
+route.get('/contato/edit/:id', requiredLogin, contatoController.edit)
+route.put('/contato/edit/:id', requiredLogin, contatoController.update)
 
 // LOGIN
 route.get('/login/', loginController.index)
